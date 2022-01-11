c0643dfa3f216b09b9f84ed4bec64b92347b72b6
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-11 09:16:56 +0100

    Create `LaunchView.swift` and update file management

--- Sheep/SheepApp.swift
+++ Sheep/SheepApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct SheepApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ Sheep/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  Sheep
+//
+//  Created by Rostislav Brož on 1/11/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.white)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
