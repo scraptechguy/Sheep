2cb4920f2b4e6d0c907af032ab16fd5989615733
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-11 09:24:17 +0100

    Move vars and functions to `ContentModel.swift`

--- Sheep/ViewModels/ContentModel.swift
+++ Sheep/ViewModels/ContentModel.swift
@@ -24,4 +24,39 @@ class ContentModel: ObservableObject {
     @Published var objectClrBlue = Color(red: 0, green: 1, blue: 0.9375)
     @Published var fontClrDark = Color(red: 0.3320, green: 0.3242, blue: 0.3242)
     @Published var fontClr = Color(red: 1, green: 1, blue: 1)
+    
+    
+    @Published var offset: CGSize = .zero
+    @Published var showingSettings:Bool = false
+    
+    
+    struct LiquidShape: Shape {
+        
+        var offset: CGSize
+        
+        var animatableData: CGSize.AnimatableData {
+            get {return offset.animatableData}
+            set {offset.animatableData = newValue}
+        }
+        
+        func path(in rect: CGRect) -> Path {
+            return Path {path in
+                let width = rect.width + (-offset.width > 0 ? offset.width: 0)
+                
+                path.move(to: CGPoint(x: 0, y: 0))
+                path.addLine(to: CGPoint(x: rect.width, y: 0))
+                path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+                path.addLine(to: CGPoint(x: 0, y: rect.height))
+                
+                let from = 80 + (offset.width)
+                path.move(to: CGPoint(x: rect.width, y: from > 80 ? 80: from))
+                
+                var to = 180 + (offset.height) + (-offset.width)
+                to = to < 180 ? 180: to
+                
+                let mid: CGFloat = 80 + ((to - 80) / 2)
+                path.addCurve(to: CGPoint(x: rect.width, y: to), control1: CGPoint(x: width - 50, y: mid), control2: CGPoint(x: width - 50, y: mid))
+            }
+        }
+    }
 }
--- Sheep/Views/LaunchView.swift
+++ Sheep/Views/LaunchView.swift
@@ -13,15 +13,12 @@ struct LaunchView: View {
      @EnvironmentObject var model: ContentModel
      
      
-     @State var offset: CGSize = .zero
-     @State var showingSettings:Bool = false
-     
      var body: some View {
          ZStack {
              model.bg.overlay(
                     HomeView()
                  )
-                 .clipShape(LiquidShape(offset: offset))
+                 .clipShape(ContentModel.LiquidShape(offset: model.offset))
                  .overlay(
                      Image(systemName: "gearshape")
                          .font(.system(size: model.screenSize.width / 18))
@@ -29,34 +26,34 @@ struct LaunchView: View {
                          .contentShape(Rectangle())
                          .gesture(DragGesture().onChanged({ (value) in
                              withAnimation(.interactiveSpring(response: 0.7, dampingFraction: 0.6, blendDuration: 0.6)){
-                                     offset = value.translation
+                                    model.offset = value.translation
                              }
                          }).onEnded({ (value) in
                              withAnimation(.spring()) {
-                                 if -offset.width > model.screenSize.width / 2 {
-                                     offset.width = -model.screenSize.height
-                                     showingSettings.toggle()
+                                 if -model.offset.width > model.screenSize.width / 2 {
+                                     model.offset.width = -model.screenSize.height
+                                     model.showingSettings.toggle()
                                  }
                                  
                                  else {
-                                     offset = .zero
+                                     model.offset = .zero
                                  }
                              }
                          })
                          )
                          .foregroundColor(model.objectsClrDark)
                          .offset(x: model.screenSize.width / 50, y: model.screenSize.width / 3.6)
-                         .opacity(offset == .zero ? 1 : 0),
+                        .opacity(model.offset == .zero ? 1 : 0),
                      alignment: .topTrailing
                  )
                  .ignoresSafeArea()
              
-             if showingSettings == true {
+             if model.showingSettings == true {
                  Text("show home")
                      .onTapGesture {
                          withAnimation(.spring()) {
-                             offset = .zero
-                             showingSettings.toggle()
+                             model.offset = .zero
+                             model.showingSettings.toggle()
                          }
                      }
              }
@@ -64,35 +61,6 @@ struct LaunchView: View {
      }
  }
 
- struct LiquidShape: Shape {
-     
-     var offset: CGSize
-     
-     var animatableData: CGSize.AnimatableData {
-         get {return offset.animatableData}
-         set {offset.animatableData = newValue}
-     }
-     
-     func path(in rect: CGRect) -> Path {
-         return Path {path in
-             let width = rect.width + (-offset.width > 0 ? offset.width: 0)
-             
-             path.move(to: CGPoint(x: 0, y: 0))
-             path.addLine(to: CGPoint(x: rect.width, y: 0))
-             path.addLine(to: CGPoint(x: rect.width, y: rect.height))
-             path.addLine(to: CGPoint(x: 0, y: rect.height))
-             
-             let from = 80 + (offset.width)
-             path.move(to: CGPoint(x: rect.width, y: from > 80 ? 80: from))
-             
-             var to = 180 + (offset.height) + (-offset.width)
-             to = to < 180 ? 180: to
-             
-             let mid: CGFloat = 80 + ((to - 80) / 2)
-             path.addCurve(to: CGPoint(x: rect.width, y: to), control1: CGPoint(x: width - 50, y: mid), control2: CGPoint(x: width - 50, y: mid))
-         }
-     }
- }
 
 struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
